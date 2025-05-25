9c1320d61a9cdb4859b6324a8a566f05c5157ff7
GitRepositorioLuz <gitHubAlmacen@gmail.com>
2025-05-25 12:44:41 -0500

    Changes in project

--- src/main/java/pe/com/app/producto/advice/RecursoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package pe.com.app.producto.advice;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
--- src/main/java/pe/com/app/producto/common/util/Constant.java
+++
@@ -1,5 +0,0 @@
-package pe.com.app.producto.common.util;
-
-public class Constant {
-    public final static String ERROR_CODE = "Error on Service";
-}
